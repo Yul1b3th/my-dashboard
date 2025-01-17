15fce216333dae39acbd862022754d9cadefa59c
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-01-17 18:20:54 +0100

    Material Bottom Sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -7,8 +7,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'my-dashboard';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const appRoot = document.querySelector('app-root');
+
+    if (appRoot) {
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          if (
+            mutation.type === 'attributes' &&
+            appRoot.getAttribute('aria-hidden') === 'true'
+          ) {
+            appRoot.removeAttribute('aria-hidden');
+          }
+        });
+      });
+
+      observer.observe(appRoot, { attributes: true });
+    }
+  }
 }
--- src/app/dashboard/pages/material/material.component.ts
+++ src/app/dashboard/pages/material/material.component.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import { OptionsBottomSheetComponent } from './ui/options-bottom-sheet/options-bottom-sheet.component';
 
 @Component({
   selector: 'app-material',
@@ -15,8 +21,15 @@ import { MatBadgeModule } from '@angular/material/badge';
     MatIconModule,
     MatButtonModule,
     MatBadgeModule,
+    MatBottomSheetModule,
   ],
   templateUrl: './material.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class MaterialComponent {}
+export default class MaterialComponent {
+  private _bottomSheet = inject(MatBottomSheet);
+
+  openBottomSheet() {
+    this._bottomSheet.open(OptionsBottomSheetComponent);
+  }
+}
